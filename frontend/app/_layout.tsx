87d4effaed66b34e5f144a4fde70c056869cd4c4
Ramzi Shodmonbekov <ramzi.shodmonbekov_2026@ucentralasia.org>
2025-04-10 16:39:09 +0600

    add animation in _layouts

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,8 +1,26 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import Animation from "./animation";
 
 export default function RootLayout() {
+  const [animationFinished, setAnimationFinished] = useState(false);
+  const router = useRouter();
+
+  const handleAnimationFinished = () => {
+    setAnimationFinished(true);
+  };
+
+  useEffect(() => {
+    if (animationFinished) {
+      router.replace("/dashboard"); // Use replace to prevent going back to the animation
+    }
+  }, [animationFinished, router]);
+
   return (
+    <>
+      {!animationFinished ? (
+        <Animation onAnimationFinished={handleAnimationFinished} />
+      ) : (
     <Stack
       screenOptions={{
         headerShown: false,
@@ -22,5 +40,7 @@ export default function RootLayout() {
       <Stack.Screen name="items/[id]" />
       <Stack.Screen name="photos/gallery" />
     </Stack>
+      )}
+    </>
   );
 }

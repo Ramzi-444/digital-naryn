63b57bed42fab3c41d7dbae093815d51a95e20a5
Ramzi Shodmonbekov <ramzi.shodmonbekov_2026@ucentralasia.org>
2025-04-10 15:27:54 +0600

    do nothing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,26 +1,47 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import Animation from "./animation";
 
 export default function RootLayout() {
+  const [animationFinished, setAnimationFinished] = useState(false);
+  const router = useRouter();
+
+  const handleAnimationFinished = () => {
+    setAnimationFinished(true);
+  };
+
+  useEffect(() => {
+    if (animationFinished) {
+      router.replace("/dashboard"); // Use replace to prevent going back to the animation
+    }
+  }, [animationFinished, router]);
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: 'slide_from_right',
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="dashboard" />
-      <Stack.Screen name="contact" />
-      <Stack.Screen 
-        name="modals/search" 
-        options={{ 
-          presentation: "modal",
-          animation: "slide_from_bottom"
-        }} 
-      />
-      <Stack.Screen name="items/[id]" />
-      <Stack.Screen name="photos/gallery" />
-    </Stack>
+    <>
+      {!animationFinished ? (
+        <Animation onAnimationFinished={handleAnimationFinished} />
+      ) : (
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            animation: "slide_from_right",
+          }}
+        >
+          <Stack.Screen name="index" />
+          <Stack.Screen name="dashboard" />
+          <Stack.Screen name="contact" />
+          <Stack.Screen
+            name="modals/search"
+            options={{
+              presentation: "modal",
+              animation: "slide_from_bottom",
+            }}
+          />
+          <Stack.Screen name="categories/[id]" />
+          <Stack.Screen name="items/[id]" />
+          <Stack.Screen name="photos/gallery" />
+        </Stack>
+      )}
+    </>
   );
 }
